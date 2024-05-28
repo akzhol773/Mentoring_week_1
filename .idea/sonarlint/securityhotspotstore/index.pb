4c59e2d2844c89df76d5cceab238d6260e8abcf9
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2024-05-28 11:10:43 +0100

    added all files

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+R
+"src/main/java/com.akzhol/Main.java,c\0\c0bf4208078b44ba50913659c8f7458ea0a13d65
---
+++ src/main/java/com.akzhol/Main.java
@@ -0,0 +1,102 @@
+package com.akzhol;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    private static HashMap<Integer, String> data = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+
+            System.out.println("-------------------------------------------------------------------------");
+            System.out.println("| Program usage:                                                         |");
+            System.out.println("| Command to create a string: CREATE {some_string}                       |");
+            System.out.println("| Command to get a string: GET or GET {id}                               |");
+            System.out.println("| Command to update a string: UPDATE {id} {new_string_value}             |");
+            System.out.println("| Command to delete a string: DELETE {id}                                |");
+            System.out.println("| Enter 'Exit' to stop the program.                                      |");
+            System.out.println("-------------------------------------------------------------------------");
+
+
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("Exit")){
+                break;
+            }
+
+            String[] parts = userInput.split(" ", 3);
+            String command = parts[0];
+
+            switch (command.toUpperCase()){
+                case "CREATE":
+                    System.out.println(handleCreate(parts));
+                    break;
+                case "GET":
+                    System.out.println(handleGet(parts));
+                    break;
+                case "UPDATE":
+                    System.out.println(handleUpdate(parts));
+                    break;
+                case "DELETE":
+                    System.out.println(handleDelete(parts));
+                    break;
+                default:
+                    System.out.println("Please, enter correct command.");;
+
+            }
+        }
+
+    }
+
+    private static String handleDelete(String[] parts) {
+        int id = Integer.parseInt(parts[1]);
+        if (parts.length != 2) {
+            return "Invalid DELETE command. Usage: DELETE {id}";
+        } else if (!data.containsKey(id)) {
+                return "String with id = " + id + " not found";
+        }
+        data.remove(id);
+        return "String with id = " + id + " deleted";
+    }
+
+    private static String handleUpdate(String[] parts) {
+        if (parts.length < 3) {
+            return "Invalid UPDATE command. Usage: UPDATE {id} {new_string_value}";
+
+        }
+        int id = Integer.parseInt(parts[1]);
+        String newValue = parts[2];
+        if (!data.containsKey(id)) {
+            return "String with id = " + id + " not found";
+        }
+        data.put(id, newValue);
+        return "String with id = " + id + " updated";
+    }
+
+
+    private static String handleGet(String[] parts) {
+        if (parts.length == 1) {
+            return data.toString();
+        } else if (parts.length == 2) {
+            int id = Integer.parseInt(parts[1]);
+            return data.get(id);
+        } else {
+            return "Invalid GET command. Usage: GET or GET {id}";
+        }
+    }
+
+    private static String handleCreate(String[] parts) {
+        if (parts.length < 2) {
+            return "Invalid CREATE command. Usage: CREATE {some_string}";
+        }
+        String value = parts[1];
+        Integer id = data.size()+1;
+        data.put(id, value);
+        return "String saved with id = " + id;
+    }
+}
